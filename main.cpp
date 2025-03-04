4db76e250798dd8dc4b2ab933f217277eb52b42e
Tiffany <tiffzhou@umich.edu>
2025-03-03 20:56:55 -0500

    sphere surface normals

--- main.cpp
+++ main.cpp
@@ -3,20 +3,28 @@
 #include "color.hpp"
 #include "ray.hpp"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 origin_center = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = dot((-2 * r.direction()), origin_center);
-    auto c = dot(origin_center, origin_center) - (radius * radius);
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), origin_center);
+    auto c = origin_center.length_squared() - (radius * radius);
     
-    auto discriminant = b * b - (4 * a * c);
-    return discriminant >= 0;
+    auto discriminant = h * h - a * c;
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (h - std::sqrt(discriminant)) / a;
+    }
 }
 
 // return color for a given scene ray
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    point3 sphere_center(0, 0, -1);
+    auto hit_point = hit_sphere(sphere_center, 0.5, r);
+    if (hit_point > 0){
+        vec3 n = unit_vector(r.at(hit_point) - sphere_center);
+        return 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1);
     }
     vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5 * (unit_dir.y() + 1.0);
