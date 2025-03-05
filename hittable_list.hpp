f8d781f1112b3ce50e2668c8df8a48af7ba97410
albonwu <albonwu@umich.edu>
2025-03-04 20:16:18 -0600

    hittable list

---
+++ hittable_list.hpp
@@ -0,0 +1,49 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.hpp"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class HittableList : public Hittable {
+    public:
+        // shared_ptr prevents object slicing
+        std::vector<shared_ptr<Hittable>> objects;
+
+        HittableList() {
+        }
+        HittableList(shared_ptr<Hittable> object) {
+            add(object);
+        }
+
+        void clear() {
+            objects.clear();
+        }
+
+        void add(shared_ptr<Hittable> object) {
+            objects.push_back(object);
+        }
+
+        bool hit(const ray &r, double ray_tmin, double ray_tmax,
+                 hit_record &rec) const override {
+            hit_record temp_rec;
+            bool any_hits = false;
+            auto closest_so_far = ray_tmax;
+
+            for (const auto &object : objects) {
+                if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+                    any_hits = true;
+                    closest_so_far = temp_rec.t;
+                    rec = temp_rec;
+                }
+            }
+
+            return any_hits;
+        }
+};
+
+#endif
