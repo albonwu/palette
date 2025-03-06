73dfa6d8fe16503cdb12615bb2f294b01e81223f
Tiffany <tiffzhou@umich.edu>
2025-03-05 22:43:13 -0500

    convert colors to gamma space

--- color.hpp
+++ color.hpp
@@ -6,10 +6,21 @@
 
 using Color = Vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+    return 0;
+}
+
 void write_color(std::ostream &out, const Color &pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static const Interval intensity(0.000, 0.999);
     int rbyte = int(256 * intensity.clamp(r));
